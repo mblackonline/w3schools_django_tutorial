47959ce90e0abcc8c3515aa029b024635b6acd84
Matt Black <mblackonline.git@gmail.com>
2023-09-21 10:52:43 -0400

    added requirements.txt and revised settings.py to allow local and production scenarios

--- cd_tech_backend/settings.py
+++ cd_tech_backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1']  # you will need to add your IP address or Domain Name here to run in production
 
 
 # Application definition
@@ -75,27 +75,24 @@ TEMPLATES = [
 WSGI_APPLICATION = 'cd_tech_backend.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('PG_DB_NAME'),
-        'USER': os.getenv('PG_DB_USER'),
-        'PASSWORD': os.getenv('PG_DB_PASSWORD'),
-        'HOST': os.getenv('PG_DB_HOST'),
-        'PORT': os.getenv('PG_DB_PORT'),
+if os.getenv('DJANGO_ENV') == 'development':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('PG_DB_NAME'),
+            'USER': os.getenv('PG_DB_USER'),
+            'PASSWORD': os.getenv('PG_DB_PASSWORD'),
+            'HOST': os.getenv('PG_DB_HOST'),
+            'PORT': os.getenv('PG_DB_PORT'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
